7eaf2c34691f6a995447af0962dee8d7c2bae465
JoshiDev <joseluisjimenez1998@gmail.com>
2021-07-29 11:28:17 -0400

    Primer Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+require('colors');
+
+const { 
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoCheckList,
+} = require('./helpers/inquirer');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+
+
+const Tareas = require('./models/tareas');
+
+
+const main = async () => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB  = leerDB();
+
+    if(tareasDB){
+        tareas.cargarTareasFromArray(tareasDB)
+    }
+    
+    do {
+        opt = await inquirerMenu();
+        
+        switch(opt){
+            case '1':
+                // Crear Opcion
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea(desc);
+
+            break;
+
+            case'2':
+            tareas.listadoCompleto();
+            break;
+
+            case'3':
+            tareas.listarPendientesCompletadas(true);
+            break;
+
+            case'4':
+            tareas.listarPendientesCompletadas(false);
+            break;
+
+            case'5':
+            const ids = await mostrarListadoCheckList(tareas.listadoArr);
+            tareas.toogleCompletadas(ids);
+            break;
+
+            case'6':
+            const id = await listadoTareasBorrar(tareas.listadoArr);
+            if(id !== '0'){
+                const ok = await confirmar('Estas seguro?');
+                if (ok){
+                    tareas.borrarTarea( id ); 
+                }
+            }
+            break;
+
+        }
+
+        guardarDB( tareas.listadoArr);
+
+        await pausa();
+
+    } while(opt !== '0' ); 
+
+
+}
+
+main();
